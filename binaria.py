61b7e166f3152219944e0b7191fb1880b5d9c1ac
mtoledoDL <mtoledo@directoriolegislativo.org>
2025-06-09 13:24:35 -0300

    add print statement

--- binaria.py
+++ binaria.py
@@ -4,21 +4,23 @@ edades = [34, 21, 45, 19, 60, 27, 38, 22, 15, 8]
 
 """ Función de ordenamiento por selección """
 
-def ordenar_edades (lista):
+
+def ordenar_edades(lista):
     n = len(lista)
-    for i in range (n):
+    for i in range(n):
         pos_menor = i
-        for j in range (i + 1, n):
-            if lista [j] < lista[pos_menor]:
+        for j in range(i + 1, n):
+            if lista[j] < lista[pos_menor]:
                 pos_menor = j
-        lista [i], lista[pos_menor] = lista [pos_menor], lista[i]
+        lista[i], lista[pos_menor] = lista[pos_menor], lista[i]
 
 
 """ Función de búsqueda binaria """
 
+
 def busqueda_binaria(lista, valor):
     inicio = 0
-    fin = len (lista) -1
+    fin = len(lista) - 1
     while inicio <= fin:
         medio = (inicio + fin) // 2
         if lista[medio] == valor:
@@ -28,6 +30,7 @@ def busqueda_binaria(lista, valor):
         else:
             fin = medio - 1
     return -1
+
 
 ordenar_edades(edades)
 print("Edades ordenadas: ", edades)
@@ -47,7 +50,9 @@ fin = timeit.default_timer()
 if resultado != -1:
     print(f"El valor {valor_buscar} fue encontrado en la posición {resultado}")
 
+    print("Tiempo de busqueda:", fin - inicio, "segundos")
+
 else:
     print(f"El valor {valor_buscar} no se encuentra en la lista")
 
-    print("Tiempo de busqueda:" , fin - inicio, "segundos")
+    print("Tiempo de busqueda:", fin - inicio, "segundos")
